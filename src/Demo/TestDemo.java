21d609e4464bb6493361d2394a80b6dd3fab716e
VeronicaYyq <55088462+VeronicaYyq@users.noreply.github.com>
2019-09-19 11:31:04 +0800

    Update TestDemo.java

--- src/Demo/TestDemo.java
+++ src/Demo/TestDemo.java
@@ -17,33 +17,11 @@ public class TestDemo {
                 s.sell();//调用资源类的方法
             }
         },"D").start();
-       /* new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                for (int i = 1; i <= 80; i++) {
-                    s.sell();//调用资源类的方法
-                }
+      new Thread(() -> {
+            for (int i = 1; i <= 80; i++) {
+                s.sell();//调用资源类的方法
             }
-        }, "A").start();
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                for (int i = 1; i <= 80; i++) {
-                    s.sell();//调用资源类的方法
-                }
-            }
-        }, "B").start();
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                for (int i = 1; i <= 80; i++) {
-                    s.sell();//调用资源类的方法
-                }
-            }
-        }, "C").start();*/
+        },"A").start();
     }
 }
 class Demo01 extends Thread{
